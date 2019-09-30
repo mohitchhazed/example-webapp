e1932765e3e5d1021e3314d357a359cf36c81006
mohitchhazed <mohitchhazed@users.noreply.github.com>
2019-09-30 23:12:11 +0530

    Update run-stack.sh

--- run-stack.sh
+++ run-stack.sh
@@ -8,5 +8,5 @@
     --parameters \
     ParameterKey=DockerImage,ParameterValue=861673107443.dkr.ecr.ap-south-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-b35673db
-    ParameterKey=Cluster,ParameterValue=default \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:ap-south-1:861673107443:listener/app/example-webapp/4a66d1981521ec9e/57a0a82f28c0399c
+    ParameterKey=Cluster,ParameterValue="default" \
+    ParameterKey=Listener,ParameterValue="arn:aws:elasticloadbalancing:ap-south-1:861673107443:listener/app/example-webapp/4a66d1981521ec9e/57a0a82f28c0399c"
